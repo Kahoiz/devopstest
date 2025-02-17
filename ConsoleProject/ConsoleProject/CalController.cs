2812a2d1f683c54a9aed07ab113ce3a3f704c958
Mathias Rasmussen <49396726+GuRLiG@users.noreply.github.com>
2025-02-17 10:46:46 +0100

    title (#35)
    * commit
    
    * title

---
+++ ConsoleProject/ConsoleProject/CalController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject
+{
+    internal class CalController
+    {
+        Calculater CAL;
+        public CalController()
+        {
+            CAL = new Calculater();
+        }
+
+        public int Add(int a, int b)
+        {
+            return CAL.Add(a, b);
+        }
+    }
+}
---
+++ ConsoleProject/TestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+using Xunit;
+using ConsoleProject;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestAccept()
+        {
+            CalController calController = new CalController();
+
+            Assert.Equal(3, calController.Add(1, 2));
+        }
+
+        [Fact]
+        public void TestFail()
+        {
+            CalController calController = new CalController();
+
+            Assert.Equal(4, calController.Add(1, 2));
+        }
+    }
+}
